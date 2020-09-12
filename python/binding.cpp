65504398f114d6a6668733d1dca5c50dcffb2ba5
yumcyawiz <hashdolf1923@gmail.com>
2020-09-13 01:21:45 +0900

    add __repr__ for vec3

--- python/binding.cpp
+++ python/binding.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "mylib.h"
 #include "pybind11/pybind11.h"
 
@@ -11,5 +13,9 @@ PYBIND11_MODULE(MyLib, m) {
   py::class_<Vec3>(m, "Vec3")
       .def(py::init<>())
       .def(py::init<float>())
-      .def(py::init<float, float, float>());
+      .def(py::init<float, float, float>())
+      .def("__repr__", [](const Vec3& v) {
+        return "(" + std::to_string(v.v[0]) + ", " + std::to_string(v.v[1]) +
+               ", " + std::to_string(v.v[2]) + ")";
+      });
 }
