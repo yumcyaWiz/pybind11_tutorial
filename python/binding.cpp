49a394f4966b08ce54fa62fdb9628dfa9ca0a57c
yumcyawiz <hashdolf1923@gmail.com>
2020-09-13 05:36:08 +0900

    add numpy binding

--- python/binding.cpp
+++ python/binding.cpp
@@ -10,10 +10,18 @@ PYBIND11_MODULE(MyLib, m) {
   m.def("func2", &func2, py::arg("i") = 1, py::arg("j") = 2);
   m.def("func3", &func3);
 
-  py::class_<Vec3>(m, "Vec3")
+  py::class_<Vec3>(m, "Vec3", py::buffer_protocol())
       .def(py::init<>())
       .def(py::init<float>())
       .def(py::init<float, float, float>())
+
+      .def_buffer([](Vec3& v) {
+        return py::buffer_info(v.v, sizeof(float),
+                               py::format_descriptor<float>::format(), 1, {3},
+                               {sizeof(float)});
+      })
+
+      .def_readonly("v", &Vec3::v)
 
       .def("__repr__",
            [](const Vec3& v) {
@@ -21,6 +29,5 @@ PYBIND11_MODULE(MyLib, m) {
                     std::to_string(v.v[1]) + ", " + std::to_string(v.v[2]) +
                     ")";
            })
-
-      .def_readonly("v", &Vec3::v);
+      .def("__getitem__", [](const Vec3& v, int i) { return v.v[i]; });
 }
--- python/test.py
+++ python/test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import MyLib
 
 print(MyLib.func1(1, 2))
@@ -7,5 +9,7 @@ x = 0
 MyLib.func3(x)
 print(x)
 
-vec = MyLib.Vec3(1, 2, 3)
-print(vec.v)
+v = MyLib.Vec3(1, 2, 3)
+print(v[1])
+
+print(v + np.array([1, 2, 3]))
